6a7ebc7f4f1c4ce104c99b5ab0b251e52196eaab
zzzzbw <zzzzbw@gmail.com>
2020-03-21 02:39:42 +0800

    :tada: Init repo

--- cloud-server-multi/multi-autoconfigure-starter/src/main/java/cn/zzzzbw/autoconfigure/starter/CacheComponentAutoConfiguration.java
+++ cloud-server-multi/multi-autoconfigure-starter/src/main/java/cn/zzzzbw/autoconfigure/starter/CacheComponentAutoConfiguration.java
@@ -1,24 +1,29 @@
-package cn.zzzzbw.provider.bootstrap.config;
+package cn.zzzzbw.autoconfigure.starter;
 
 import cn.zzzzbw.server.common.component.CacheComponent;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.core.StringRedisTemplate;
 
 /**
+ * CacheComponent自动配置类
+ *
  * @author by zzzzbw
- * @since 2020/3/21 1:20
+ * @since 2020/3/21 2:02
  */
 @Configuration
-public class CacheConfig {
+@ConditionalOnClass({CacheComponent.class})
+public class CacheComponentAutoConfiguration {
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
     @Bean
+    @ConditionalOnMissingBean(CacheComponent.class)
     public CacheComponent cacheComponent() {
         return new CacheComponent(stringRedisTemplate);
     }
-
 }
--- cloud-server-multi/multi-consumer/src/main/java/cn/zzzzbw/server/consumer/config/SwaggerConfig.java
+++ cloud-server-multi/multi-consumer/src/main/java/cn/zzzzbw/server/consumer/config/SwaggerConfig.java
@@ -32,5 +32,4 @@ public class SwaggerConfig {
                 .version("1.0")
                 .build();
     }
-
 }
