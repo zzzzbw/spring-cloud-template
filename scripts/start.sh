b7a4efdb91e8348b2f6b860f2d36e7b874b05011
zzzzbw <zzzzbw@gmail.com>
2020-03-30 20:03:36 +0800

    :tada: Init repo

---
+++ scripts/start.sh
@@ -0,0 +1,16 @@
+FULLPATH=$(cd "$(dirname "$0")"; pwd)/$1
+  
+echo "start server: $FULLPATH"
+
+JVMARGS=" -Dfile.encoding=UTF-8 "
+
+ENV=$2
+if [ ! -n "$2" ] ;then
+    echo "you have not input a env!"
+    exit 1
+fi
+
+
+echo "nohup java -Denv=$ENV $JVMARGS -jar $FULLPATH --spring.profiles.active=$ENV 1>/dev/null 2>$FULLPATH.error.log &"
+
+nohup java -Denv=$ENV $JVMARGS -jar $FULLPATH --spring.profiles.active=$ENV 1>/dev/null 2>$FULLPATH.error.log &
